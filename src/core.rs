bc0f6a7ef8afc5566d6273aa6449fdbd5f93d42c
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-04-28 02:43:13 -0400

    was copying off wrong branch, start with lexer tomorrow

--- src/core.rs
+++ src/core.rs
@@ -1,4 +1,8 @@
+use regex::Regex;
 use std::io::{self, Read, Write};
+
+const STATEMENT_REGEX: &str = r";";
+const TOKEN_REGEX: &str = r"([_a-zA-Z][_a-zA-Z0-9]*)|([0-9]*[0-9.][0-9]*)|([:=+*\/;.\{\}\(\)\-])";
 
 pub struct Interpreter {
   line_number: u64,
@@ -29,9 +33,49 @@ impl Interpreter {
     Ok(())
   }
 
-  pub fn run_string(&self, line: String) {}
+  pub fn run_string(&self, line: String) {
+    let statements = split_statements(line);
 
-  pub fn is_active(&self) -> bool {
-    self.is_active
+    for statement in statements.iter() {
+      let tokens = tokenize_statement(String::from(statement));
+    }
+  }
+
+  pub fn run_script(&self, script: String) {
+    for line in script.lines() {
+      self.run_string(String::from(line));
+    }
+  }
+
+  pub fn interactive_mode(&mut self) -> io::Result<()> {
+    while self.is_active {
+      self.read_from_stdin()?;
+    }
+
+    Ok(())
+  }
+}
+
+fn tokenize_statement(statement: String) -> Vec<Token> {
+  let delim = Regex::new(TOKEN_REGEX).unwrap();
+  delim.split(&statement).map(|t| Token::new(String::from(t))).collect()
+}
+
+fn split_statements(line: String) -> Vec<String> {
+  let delim = Regex::new(STATEMENT_REGEX).unwrap();
+  delim.split(&line).map(|s| String::from(s)).collect()
+}
+
+enum TokenType {
+  Integer(i64),
+  Float(f64),
+  Str(String),
+}
+
+struct Token {}
+
+impl Token {
+  fn new(token: String) -> Token {
+    Token {}
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 use simple_script::core::Interpreter;
+use std::env;
 use std::io;
+use std::fs;
+use std::path::Path;
+
+fn help() -> String {
+  format!("{}\n", "Usage: iss [optional script]")
+}
 
 fn main() -> io::Result<()> {
-  let mut interpreter = Interpreter::new();
+  let mut i = Interpreter::new();
 
-  while interpreter.is_active() {
-    interpreter.read_from_stdin()?;
+  let args: Vec<String> = env::args().collect();
+
+  if args.len() > 2 {
+    println!("{}", help());
+  } else if args.len() == 2 {
+    let p = Path::new(&args[1]);
+    if p.exists() {
+      let contents = fs::read_to_string(p)?;
+      i.run_script(contents);
+    }
+  } else {
+    i.interactive_mode()?;
   }
 
   Ok(())
